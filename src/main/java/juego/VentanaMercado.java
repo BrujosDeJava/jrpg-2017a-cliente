fe863ccc81927f845b7c13a8d59bf9587d92d592
RLurenzzo <rama.eze.12@gmail.com>
2017-07-06 00:59:23 -0300

    Se modifica la ventana del mercado (Todavia sirve..)

--- src/main/java/juego/VentanaMercado.java
+++ src/main/java/juego/VentanaMercado.java
@@ -3,10 +3,14 @@ package juego;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 import dominio.Item;
 import dominio.Mercado;
@@ -14,12 +18,14 @@ import mensajeria.PaqueteMercado;
 
 import javax.swing.JList;
 import javax.swing.JTextPane;
+import javax.swing.ListSelectionModel;
 import javax.swing.WindowConstants;
+import javax.swing.JScrollPane;
 
 public class VentanaMercado extends JFrame {
 
 	private JPanel contentPane;
-	private JTextPane textPane;
+	private JList<Item> listaDeItems;
 
 	/**
 	 * Launch the application.
@@ -48,19 +54,48 @@ public class VentanaMercado extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		textPane = new JTextPane();
-		textPane.setBounds(23, 11, 177, 353);
-		contentPane.add(textPane);
+    DefaultListModel<Item> listModel = new DefaultListModel<>();
+    /// Aca adentro hay que meter la lista de los usuarios conectados para luego seleccionarlos y chatear con ellos
+    Item aux = new Item(1,1);
+    aux.setNombre("Raul");
+    Item aux1 = new Item(1,1);
+    aux.setNombre("Pepe");
+    Item aux2 = new Item(1,1);
+    aux.setNombre("Tuti");
+    listModel.addElement(aux);
+    listModel.addElement(aux1);
+    listModel.addElement(aux2);
+    /// Aca adentro hay que meter la lista de los usuarios conectados para luego seleccionarlos y chatear con ellos
+    listaDeItems = new JList<>(listModel);
+    getContentPane().add(listaDeItems);
+    listaDeItems.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+    
+    listaDeItems.addListSelectionListener(new ListSelectionListener() {
+      @Override
+      public void valueChanged(ListSelectionEvent e) {
+        if (!e.getValueIsAdjusting()) {
+          /// Aca adentro hay que meter un codigo que abra un chat privado con este usuario seleccionado
+          
+          final List<Item> selectedValuesList = listaDeItems.getSelectedValuesList();
+          System.out.println(selectedValuesList);
+          /// Aca adentro hay que meter un codigo que abra un chat privado con este usuario seleccionado
+        }
+      }
+    });
+		
+		JScrollPane scrollPane = new JScrollPane(listaDeItems);
+		scrollPane.setBounds(10, 11, 323, 353);
+		contentPane.add(scrollPane);
 	}
 	
 	public void actualizarMercado(PaqueteMercado pm){
 		
-		Mercado merca = pm.getMercado();
-		textPane.setText("");
-		for(ArrayList<Item> lista : merca.getMochilas().values()){
-			for(Item i: lista)
-			textPane.setText(textPane.getText()+i.getNombre()+System.lineSeparator());
-		}
-		
+//		Mercado merca = pm.getMercado();
+//		textPane.setText("");
+//		for(ArrayList<Item> lista : merca.getMochilas().values()){
+//			for(Item i: lista)
+//			textPane.setText(textPane.getText()+i.getNombre()+System.lineSeparator());
+//		}
+//		
 	}
 }
